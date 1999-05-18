beb9b2818b015267a95f1cb13b13ccf422a9990b
swartz <swartz@878abba1-507e-414f-af30-0bba99d2f2a9>
1999-05-18 01:58:35 +0000

    don't use ApacheHandler unless we are in mod_perl or debug file environment
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@112 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason.pm
+++ lib/HTML/Mason.pm
@@ -7,6 +7,15 @@ $HTML::Mason::VERSION = '0.4';
 
 use HTML::Mason::Parser;
 use HTML::Mason::Interp;
-use HTML::Mason::ApacheHandler;
-use HTML::Mason::Preview;
+
+if ($ENV{MOD_PERL} || $HTML::Mason::IN_DEBUG_FILE) {
+    require HTML::Mason::ApacheHandler;
+} else {
+    *HTML::Mason::ApacheHandler::new = sub {
+	die "ApacheHandler class is not defined.  If you are in a mod_perl environment, this should have been brought in automatically.  Send a bug report to the Mason development team, and for now, try using HTML::Mason::ApacheHandler manually in your handler.pl.\n";
+    }
+}
+
+
+
 1;
