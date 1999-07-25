1d205861c4bc293bfc576afde095773e9baf967e
swartz <swartz@878abba1-507e-414f-af30-0bba99d2f2a9>
1999-07-25 21:20:58 +0000

    version no. update
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@220 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason.pm
+++ lib/HTML/Mason.pm
@@ -3,7 +3,7 @@ package HTML::Mason;
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
-$HTML::Mason::VERSION = '0.6';
+$HTML::Mason::VERSION = '0.7';
 
 use HTML::Mason::Parser;
 use HTML::Mason::Interp;
