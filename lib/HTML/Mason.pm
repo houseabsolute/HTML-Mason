c286bf7fb2af069402616caa921beb910bc25574
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2000-11-29 07:34:59 +0000

    sync up with changes in 0.8.  Also make minimum Perl version 5.005
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@663 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason.pm
+++ lib/HTML/Mason.pm
@@ -3,19 +3,11 @@ package HTML::Mason;
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
-use 5.004;
+use 5.005;
 
 $HTML::Mason::VERSION = '1.1';
 
 use HTML::Mason::Parser;
 use HTML::Mason::Interp;
-
-if ($ENV{MOD_PERL} || $HTML::Mason::IN_DEBUG_FILE) {
-    eval 'use HTML::Mason::ApacheHandler'; die $@ if $@;
-} else {
-    *HTML::Mason::ApacheHandler::new = sub {
-	die "HTML::Mason::ApacheHandler class is not defined.  If you are in a mod_perl environment, this should have been brought in automatically.  Send a bug report to the Mason development team, and for now, try using HTML::Mason::ApacheHandler manually in your handler.pl.\n";
-    }
-}
 
 1;
