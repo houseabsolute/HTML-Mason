01e4d9df5a12f208460ad52db9e9ab984f81734a
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2001-11-07 17:41:50 +0000

    should work with HEAD requests and whatnot now
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@1315 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/Utils.pm
+++ lib/HTML/Mason/Utils.pm
@@ -31,12 +31,12 @@ sub cgi_request_args
 
     my %args;
 
-    # Checking scalar $r->args when the method is POST is important
-    # because otherwise ->url_param returns a parameter named
-    # 'keywords' with a value of () (empty array).  This is apparently
-    # a feature related to <ISINDEX> queries or something (see the
-    # CGI.pm) docs.  It makes my head heart. - dave
-    my @methods = $method eq 'GET' || ! $ENV{QUERY_STRING} ? ( 'param' ) : ( 'param', 'url_param' );
+    # Checking that there really is no query string when the method is
+    # not POST is important because otherwise ->url_param returns a
+    # parameter named 'keywords' with a value of () (empty array).
+    # This is apparently a feature related to <ISINDEX> queries or
+    # something (see the CGI.pm) docs.  It makes my head heart. - dave
+    my @methods = $method ne 'POST' || ! $ENV{QUERY_STRING} ? ( 'param' ) : ( 'param', 'url_param' );
     foreach my $key ( map { $q->$_() } @methods ) {
 	next if exists $args{$key};
 	my @values = map { $q->$_($key) } @methods;
