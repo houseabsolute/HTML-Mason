d7ec0bd78a4b6b9912fa61ec4ff44a7058d73a8e
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-01-06 19:22:45 +0000

    fix syntax error
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@1394 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/Interp.pm
+++ lib/HTML/Mason/Interp.pm
@@ -599,7 +599,7 @@ sub write_system_log {
     my $self = shift;
 
     if ($self->{system_log_fh} && $self->{system_log_events_hash}->{$_[0]}) {
-	my $time = (defined %Time::HiRes::) ? scalar(Time::HiRes::gettimeofday()) : time);
+	my $time = (defined %Time::HiRes:: ? scalar(Time::HiRes::gettimeofday()) : time);
 	my $fh = $self->{system_log_fh};
 	print $fh (join ($self->system_log_separator,
 			 $time,                  # current time
