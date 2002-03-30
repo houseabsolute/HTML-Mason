0edf66ee57d3832c72371df6f76677500c58ddc6
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-03-30 07:25:54 +0000

    use perldoc -l per Ken's suggestion
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@1937 878abba1-507e-414f-af30-0bba99d2f2a9

--- install/delete_old_pods.pl
+++ install/delete_old_pods.pl
@@ -2,23 +2,14 @@
 
 use strict;
 
-use File::Find;
-use File::Spec;
-
-my $install_dir = shift
-    or die "No install directory provided";
-
-find( \&del_old_pods, File::Spec->catdir( $install_dir, 'HTML', 'Mason' ) );
-
-sub del_old_pods
+foreach ( qw( Interp ApacheHandler Request Component ) )
 {
-    return 1 unless $_ =~ /\.pm$/;
+    my $pod_file = `perldoc -l HTML::Mason::$_`;
+    chomp $pod_file;
 
-    (my $pod_file = $File::Find::name) =~ s/\.pm$/\.pod/;
-
-    if ( -e $pod_file )
+    if ( $pod_file =~ /\.pod$/ )
     {
-	unlink $pod_file or die "Cannot remove $pod_file: $!"
+	print "Found $pod_file\n";
+	unlink $pod_file or die "Cannot unlink $pod_file: $!";
     }
 }
-
