b33b2258ecffce82071103dd3711aaab62e4ceeb
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-04-06 17:34:33 +0000

    add a quick note
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@1990 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/Parser.pm
+++ lib/HTML/Mason/Parser.pm
@@ -3,3 +3,14 @@ die "The Parser module is no longer a part of HTML::Mason.  Please see ".
     "the Lexer and Compiler modules, its replacements.\n";
 
 1;  # heh
+
+__END__
+
+=pod
+
+=head1 PARSER
+
+The Parser object is no longer part of Mason.  It has been replaced by
+the Compiler and Lexer objects.
+
+=cut
