0f6674656ff98fcfa07f3cf61aa6f094d69ab9bc
jswartz <jswartz@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-04-09 00:30:46 +0000

    Greatly simplify handler.pl
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2001 878abba1-507e-414f-af30-0bba99d2f2a9

--- eg/handler.pl
+++ eg/handler.pl
@@ -1,52 +1,26 @@
 #!/usr/bin/perl
 #
-# This is a basic, fairly fuctional Mason handler.pl.
+# A basic, fuctional Mason handler.pl.
 #
-# For something a little more involved, check out session_handler.pl
-
 package MyMason::MyApp;
 
-# Bring in main Mason package.
-use HTML::Mason;
-
-# Bring in ApacheHandler, necessary for mod_perl integration.
-# Uncomment the second line (and comment the first) to use
-# Apache::Request instead of CGI.pm to parse arguments.
+# Bring in Mason with Apache support.
 use HTML::Mason::ApacheHandler;
-
-# Uncomment the next line if you plan to use the Mason previewer.
-#use HTML::Mason::Preview;
-
 use strict;
 
-# List of modules that you want to use from components (see Admin
-# manual for details)
-#{  package HTML::Mason::Commands;
-#   use CGI;
-#}
+# List of modules that you want to use within components.
+{ package HTML::Mason::Commands;
+  use Data::Dumper;
+}
 
-# Create ApacheHandler object
-#
-my $ah = new HTML::Mason::ApacheHandler( comp_root => '<component root>',
-                                         data_dir => '<data directory>' );
-
-# Activate the following if running httpd as root (the normal case).
-# Resets ownership of all files created by Mason at startup.
-#
-#chown (Apache->server->uid, Apache->server->gid, $ah->interp->files_written);
+# Create ApacheHandler object at startup.
+my $ah = new HTML::Mason::ApacheHandler();
 
 sub handler
 {
     my ($r) = @_;
 
-    # If you plan to intermix images in the same directory as
-    # components, activate the following to prevent Mason from
-    # evaluating image files as components.
-    #
-    #return -1 if $r->content_type && $r->content_type !~ m|^text/|i;
-    
     my $status = $ah->handle_request($r);
-    
     return $status;
 }
 
