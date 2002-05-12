1a62b43f8cc4a27c96ae615f266a7749a0e0be78
jswartz <jswartz@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-05-12 14:38:03 +0000

    update standalone script link
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2100 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/CGIHandler.pm
+++ lib/HTML/Mason/CGIHandler.pm
@@ -220,9 +220,9 @@ about the particular details of invoking Mason on each request.
 
 If you want to use Mason components from I<within> a regular CGI
 script (or any other Perl program, for that matter), then you don't
-need this module.  You can simply follow the directions in L<the
-STANDALONE MODE section in
-HTML::Mason::Interp|HTML::Mason::Interp/"STANDALONE MODE">.
+need this module.  You can simply follow the directions in
+L<HTLM::Mason::Admin/Using Mason from a standalone script|"Using Mason
+from a standalone script">.    
 
 This module also provides an C<$r> request object for use inside
 components, similar to the Apache request object under
@@ -266,9 +266,9 @@ as a templating language but not an application server.
 
 C<handle_component()> will create a CGI query object, parse the query
 parameters, and send the HTTP header and component output to STDOUT.
-If you want to handle those parts yourself, see L<Using Mason from a
-standalone script in HTML::Mason::Interp|HTML::Mason::Interp/"Using
-Mason from a standalone script">.
+If you want to handle those parts yourself, see L<Using
+Mason from a standalone script|HTML::Mason::Interp/"Using Mason
+from a standalone script">.
 
 =item * handle_cgi_object()
 
