439f647440d33d029000af8ef972eb6edb5237c7
jswartz <jswartz@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-05-30 16:45:27 +0000

    Add "use HTML::Mason;" to bring in $VERSION, the assumption being that almost everyone who uses Mason will have to use the Interp.
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2163 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/Interp.pm
+++ lib/HTML/Mason/Interp.pm
@@ -10,6 +10,7 @@ use Config;
 use File::Basename;
 use File::Path;
 use File::Spec;
+use HTML::Mason;
 use HTML::Mason::Request;
 use HTML::Mason::Resolver::File;
 use HTML::Mason::Tools qw(make_fh read_file taint_is_on load_pkg);
