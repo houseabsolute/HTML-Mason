d818f57808608da2aed14f53e3537152c68358cc
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-11-22 06:46:17 +0000

    calling ->can only works on objects, not on any old scalar
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2784 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/ComponentSource.pm
+++ lib/HTML/Mason/ComponentSource.pm
@@ -56,7 +56,7 @@ sub comp_source
 
     if (my $err = $@)
     {
-	$err->can('rethrow') ? $err->rethrow : error $err;
+	UNIVERSAL::can( $err, 'rethrow' ) ? $err->rethrow : error $err;
     }
 
     unless ( defined $source )
