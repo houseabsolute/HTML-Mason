56d69f02f6369d5f894cf0fd089246e77103ef3c
kwilliams <kwilliams@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-11-23 04:21:10 +0000

    Don't clean up when debugging
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2815 878abba1-507e-414f-af30-0bba99d2f2a9

--- t/single_test.pl
+++ t/single_test.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 foreach (@ARGV) {
-  my @command = ('make', 'test', "TEST_FILES=$_", 'TEST_VERBOSE=1', 'MASON_VERBOSE=1');
+  my @command = ('make', 'test', "TEST_FILES=$_", 'TEST_VERBOSE=1', 'MASON_VERBOSE=1', 'MASON_NO_CLEANUP=1');
   print "@command\n";
   system @command;
 }
