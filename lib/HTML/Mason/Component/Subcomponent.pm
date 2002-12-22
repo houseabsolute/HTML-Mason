73e8b78db4b270f9147474d9be34f07d55b92746
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2002-12-22 20:37:32 +0000

    get rid of long-deprecated (and not documented for ages) parent_comp method
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@2909 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/Component/Subcomponent.pm
+++ lib/HTML/Mason/Component/Subcomponent.pm
@@ -30,7 +30,6 @@ sub dir_path { return $_[0]->owner->dir_path }
 sub is_subcomp { 1 }
 sub object_file { return $_[0]->owner->object_file }
 sub parent { return $_[0]->owner->parent }
-sub parent_comp { return $_[0]->{owner} }  # deprecated, replaced by owner
 sub path { return $_[0]->owner->path . ":" . $_[0]->name }
 sub persistent { return $_[0]->owner->persistent }
 sub title { return $_[0]->owner->title . ":" . $_[0]->name }
