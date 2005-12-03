52d54e4744cbb230c19da4aa18f9c14b552ac556
autarch <autarch@878abba1-507e-414f-af30-0bba99d2f2a9>
2005-12-03 20:24:32 +0000

    treat return value from comp execution like a string.  add an HTTP_OK constant instead of just using 200
    git-svn-id: file:///home/autarch/tmp/Mason/trunk@3721 878abba1-507e-414f-af30-0bba99d2f2a9

--- lib/HTML/Mason/ApacheHandler.pm
+++ lib/HTML/Mason/ApacheHandler.pm
@@ -57,6 +57,7 @@ use HTML::Mason::Exceptions( abbr => [qw(param_error error)] );
 
 use constant APACHE2    => ($mod_perl2::VERSION || $mod_perl::VERSION || 0) >= 1.999022;
 use constant OK         => 0;
+use constant HTTP_OK    => 200;
 use constant DECLINED   => -1;
 use constant NOT_FOUND  => 404;
 use constant REDIRECT   => 302;
@@ -175,12 +176,12 @@ sub exec
         and !APACHE2
         and $self->auto_send_headers
         and !HTML::Mason::ApacheHandler::http_header_sent($r)
-        and (!$retval or $retval==200)) {
+        and (!$retval or $retval eq HTTP_OK)) {
         $r->send_http_header();
     }
 
-    # mod_perl-1 treats 200 and OK the same, but mod_perl-2 does not.
-    return defined($retval) && $retval!=200 ? $retval : OK;
+    # mod_perl 1 treats HTTP_OK and OK the same, but mod_perl-2 does not.
+    return defined $retval && $retval ne HTTP_OK ? $retval : OK;
 }
 
 #
@@ -236,6 +237,7 @@ Params::Validate::validation_options( on_fail => sub { param_error( join '', @_ 
 
 use constant APACHE2    => ($mod_perl2::VERSION || $mod_perl::VERSION || 0) >= 1.999022;
 use constant OK         => 0;
+use constant HTTP_OK    => 200;
 use constant DECLINED   => -1;
 use constant NOT_FOUND  => 404;
 use constant REDIRECT   => 302;
@@ -881,7 +883,7 @@ sub prepare_request
         my $retval = ( isa_mason_exception($err, 'Abort')   ? $err->aborted_value  :
                        isa_mason_exception($err, 'Decline') ? $err->declined_value :
                        rethrow_exception $err );
-        $retval = OK if defined $retval && $retval == 200;
+        $retval = OK if defined $retval && $retval eq HTTP_OK;
         unless ($retval) {
             unless (APACHE2) {
                 unless (http_header_sent($r)) {
