aed312303bfc670daf059119c6bebcebbfe4316d
Dave Rolsky <autarch@urth.org>
2014-11-14 21:42:09 -0600

    Tidy some ancient code in bin

--- bin/convert0.6.pl
+++ bin/convert0.6.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 
 use Data::Dumper;
-use File::Find;	   
+use File::Find;
 use Getopt::Std;
 use IO::File;
 use strict;
 
-my ($EXCLUDE, $HELP, $LOWER, $QUIET, $TEST, $UPPER);
+my ( $EXCLUDE, $HELP, $LOWER, $QUIET, $TEST, $UPPER );
 
 my $usage = <<EOF;
 Usage: $0 -hlqtu [-e <regexp>] <directory> [<directory>...]
@@ -51,109 +51,133 @@ Warning: All directories will be traversed recursively.  Files are
 modified destructively and no automatic backups are created.
 EOF
 
-sub usage
-{
+sub usage {
     print $usage;
     exit;
 }
 
-sub main
-{
+sub main {
     my (%opts);
-    getopts('e:hlqtu',\%opts);
-    ($EXCLUDE, $HELP, $LOWER, $QUIET, $TEST, $UPPER) = @opts{qw(e h l q t u)};
-    if ($HELP) { print "$helpmsg\n$usage"; exit }
-    if (!@ARGV) { print "$usage\n$helpmsg"; exit }
+    getopts( 'e:hlqtu', \%opts );
+    ( $EXCLUDE, $HELP, $LOWER, $QUIET, $TEST, $UPPER )
+        = @opts{qw(e h l q t u)};
+    if ($HELP)    { print "$helpmsg\n$usage"; exit }
+    if ( !@ARGV ) { print "$usage\n$helpmsg"; exit }
     my @dirs = @ARGV;
-    
-    if (!$TEST) {
-	print "*** Mason 0.6 Conversion ***\n\n";
-	print "Quiet mode.\n" if defined($QUIET);
-	print "Excluding paths matching ($EXCLUDE).\n" if defined($EXCLUDE);
-	print "Processing ".(@dirs==1 ? "directory " : "directories ").join(",",@dirs)."\n";
-	print $warning;
-	print "\nProceed? [n] ";
-	exit if ((my $ans = <STDIN>) !~ /[Yy]/);
+
+    if ( !$TEST ) {
+        print "*** Mason 0.6 Conversion ***\n\n";
+        print "Quiet mode.\n"                          if defined($QUIET);
+        print "Excluding paths matching ($EXCLUDE).\n" if defined($EXCLUDE);
+        print "Processing "
+            . ( @dirs == 1 ? "directory " : "directories " )
+            . join( ",", @dirs ) . "\n";
+        print $warning;
+        print "\nProceed? [n] ";
+        exit if ( ( my $ans = <STDIN> ) !~ /[Yy]/ );
     }
     my $sub = sub {
-	if (-f $_ && -s _) {
-	    return if defined($EXCLUDE) && "$File::Find::dir/$_" =~ /$EXCLUDE/i;
-	    convert($_,"$File::Find::dir/$_");
-	}
+        if ( -f $_ && -s _ ) {
+            return
+                if defined($EXCLUDE) && "$File::Find::dir/$_" =~ /$EXCLUDE/i;
+            convert( $_, "$File::Find::dir/$_" );
+        }
     };
-    find($sub,@dirs);
+    find( $sub, @dirs );
 }
 
-sub convert
-{
-    my ($file,$path) = @_;
+sub convert {
+    my ( $file, $path ) = @_;
     my $buf;
     my $infh = new IO::File $file;
-    if (!$infh) { warn "cannot read $path: $!"; return }
+    if ( !$infh ) { warn "cannot read $path: $!"; return }
     { local $/ = undef; $buf = <$infh> }
 
     my $c = 0;
     my @changes;
-    my $report = sub { push(@changes,"$_[0]  -->  $_[1]") };
+    my $report = sub { push( @changes, "$_[0]  -->  $_[1]" ) };
 
     #
     # Convert section names to short versions
     #
     my $pat = "<(/?%)perl_(args|cleanup|doc|init|once|text)>";
-    if (!$TEST) {
-	if ($UPPER) {
-	    $c += ($buf =~ s{$pat}{"<$1".uc($2).">"}geio);
-	} elsif ($LOWER) {
-	    $c += ($buf =~ s{$pat}{"<$1".lc($2).">"}geio);
-	} else {
-	    $c += ($buf =~ s{$pat}{<$1$2>}gio);
-	}
-    } else {
-	while ($buf =~ m{($pat)}gio) {
-	    $report->($1,"<$2".($UPPER ? uc($3) : $LOWER ? lc($3) : $3).">");
-	}
+    if ( !$TEST ) {
+        if ($UPPER) {
+            $c += ( $buf =~ s{$pat}{"<$1".uc($2).">"}geio );
+        }
+        elsif ($LOWER) {
+            $c += ( $buf =~ s{$pat}{"<$1".lc($2).">"}geio );
+        }
+        else {
+            $c += ( $buf =~ s{$pat}{<$1$2>}gio );
+        }
+    }
+    else {
+        while ( $buf =~ m{($pat)}gio ) {
+            $report->(
+                $1,
+                "<$2" . ( $UPPER ? uc($3) : $LOWER ? lc($3) : $3 ) . ">"
+            );
+        }
     }
 
     #
     # Convert <% mc_comp ... %> to <& ... &>
     #
-    if (!$TEST) {
-	$c += ($buf =~ s{<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g);
-	$c += ($buf =~ s{<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g);
-	$c += ($buf =~ s{<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g);
-	$c += ($buf =~ s{<%\s*mc_comp\s*\(\s*(.*?)\s*\)\s*%>} {<& $1 &>}g);
-    } else {
-	while ($buf =~ m{(<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>)}g) {
-	    $report->($1,"<& $2$3 &>");
-	}
-	$buf =~ s{<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
-	while ($buf =~ m{(<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>)}g) {
-	    $report->($1,"<& $2$3 &>");
-	}
-	$buf =~ s{<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
-	while ($buf =~ m{(<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>)}g) {
-	    $report->($1,"<& $2$3 &>");
-	}
-	$buf =~ s{<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
-        while ($buf =~ m{(<%\s*mc_comp\s*\((.*?)\s*\)\s*%>)}g) {
-	    $report->($1,"<& $2 &>");
-	}
+    if ( !$TEST ) {
+        $c
+            += ( $buf
+                =~ s{<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g
+            );
+        $c
+            += ( $buf
+                =~ s{<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g
+            );
+        $c
+            += ( $buf
+                =~ s{<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g
+            );
+        $c += ( $buf =~ s{<%\s*mc_comp\s*\(\s*(.*?)\s*\)\s*%>} {<& $1 &>}g );
+    }
+    else {
+        while (
+            $buf =~ m{(<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>)}g )
+        {
+            $report->( $1, "<& $2$3 &>" );
+        }
+        $buf
+            =~ s{<%\s*mc_comp\s*\(\s*\'([^\']+)\'\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
+        while ( $buf
+            =~ m{(<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>)}g ) {
+            $report->( $1, "<& $2$3 &>" );
+        }
+        $buf
+            =~ s{<%\s*mc_comp\s*\(\s*\"([^\"\$]+)\"\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
+        while (
+            $buf =~ m{(<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>)}g )
+        {
+            $report->( $1, "<& $2$3 &>" );
+        }
+        $buf
+            =~ s{<%\s*mc_comp\s*\(\s*(\"[^\"]+\")\s*(.*?)\s*\)\s*%>} {<& $1$2 &>}g;
+        while ( $buf =~ m{(<%\s*mc_comp\s*\((.*?)\s*\)\s*%>)}g ) {
+            $report->( $1, "<& $2 &>" );
+        }
     }
 
     if ($TEST) {
-	if (@changes) {
-	    print scalar(@changes)." substitutions in $path:\n";
-	    print join("\n",@changes)."\n\n";
-	}
+        if (@changes) {
+            print scalar(@changes) . " substitutions in $path:\n";
+            print join( "\n", @changes ) . "\n\n";
+        }
     }
-    
-    if ($c && !$TEST) {
-	print "$c substitutions in $path\n" if !$QUIET;
-	my $outfh = new IO::File ">$file";
-	if (!$outfh) { warn "cannot write $path: $!"; return }
-	$outfh->print($buf);
+
+    if ( $c && !$TEST ) {
+        print "$c substitutions in $path\n" if !$QUIET;
+        my $outfh = new IO::File ">$file";
+        if ( !$outfh ) { warn "cannot write $path: $!"; return }
+        $outfh->print($buf);
     }
 }
-
 
 main();
